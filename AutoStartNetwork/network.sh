429e087e269803343c3318a5ff2a29d29d00d64c
YJC <openopentw@gmail.com>
2018-03-02 19:24:07 +0800

    Make network.sh sleep 30 before checking network

--- AutoStartNetwork/network.sh
+++ AutoStartNetwork/network.sh
@@ -1,4 +1,12 @@
+echo -n "Waiting for computer to completely boot "
+for((i = 0; i < 10; ++i)); do
+    sleep 3
+    echo -n .
+done
+
+echo ""
+sleep 20
 in_dorm=`ipconfig | grep 140.112.247.82`
 if [[ ! -z $in_dorm ]]; then
-	netsh wlan start hostednetwork
+    netsh wlan start hostednetwork
 fi
